a2fa2b29f7f9e8bbedfac3813854cf0fc5fa9298
Yllan Gurgel <the_yllan@hotmail.com>
2023-03-21 22:24:35 -0300

    adding the input for creating a new task

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import { Header } from './components/Header'
 import './global.css'
+import styles from './App.module.css'
+import { PlusCircle } from 'phosphor-react'
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <form className={styles.searchForm}>
+          <input type="text" placeholder='Adicione uma nova tarefa' />
+          <button type='submit'>
+            Criar
+            <PlusCircle size={20} />
+          </button>
+        </form>
+      </div>
+      
+    </>
   )
 }
 
